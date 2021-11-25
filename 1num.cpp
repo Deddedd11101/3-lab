f4b9dd18b65496b1aa34cca4d03ce01ea08788b9
Востриков Антон <93368273+Deddedd11101@users.noreply.github.com>
2021-11-26 01:54:24 +0300

    Add files via upload

---
+++ 1num.cpp
@@ -0,0 +1,23 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int i, sum;
+   // i = 1; sum = 0;
+   // for (; i <= 100; i++) sum += i; верно
+
+    // sum = 0;
+    // for (i = 1; i <= 100;) sum += i++; верно
+
+   // for (i = 1, sum = 0; i <= 100; sum += i + , i++); ошибка синтаксиса
+
+   // for (i = 1, sum = 0; i <= 100; sum += i++); верно 
+
+   // for (i = 0, sum = 0; i++, i <= 100; sum += i); верно
+    std::cout << sum;
+}
+
+
